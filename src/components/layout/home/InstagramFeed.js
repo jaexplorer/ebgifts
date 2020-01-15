11badbeff27248a49dc6e3ab1c168b31d419c549
Andrew <andrewsabato8@gmail.com>
2020-01-16 00:04:09 +1100

    debugging Instagram

--- src/components/layout/home/InstagramFeed.js
+++ src/components/layout/home/InstagramFeed.js
@@ -8,8 +8,17 @@ const InstagramFeed = () => {
     try {
       const res = await axios.get(`${process.env.INSTAGRAM_KEY}`);
       setFeed(res.data.data);
-    } catch (err) {
-      console.log(err);
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log("Error", error.message);
+      }
+      console.log(error);
     }
   };
 
