86ed239d3072b2537c07d9c6787c695bb4241357
Andrew <andrewsabato8@gmail.com>
2020-01-16 01:13:22 +1100

    debugging Instagram

--- src/components/layout/home/InstagramFeed.js
+++ src/components/layout/home/InstagramFeed.js
@@ -5,30 +5,8 @@ import axios from "axios";
 const InstagramFeed = () => {
   const [feed, setFeed] = useState(false);
   const getFeed = async () => {
-    try {
-      const res = await axios.get(
-        "https://api.instagram.com/v1/users/self/media/recent/?access_token=21253163367.1677ed0.16e4941ab2f04e6da1697bf6775f58ee"
-      );
-      console.log(res);
-      console.log(res.data);
-      setFeed(res.data.data);
-      console.log("Here0");
-    } catch (error) {
-      if (error.response) {
-        console.log("Here1");
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        console.log("Here2");
-        console.log(error.request);
-      } else {
-        console.log("Here3");
-        console.log("Error", error.message);
-      }
-      console.log("Here4");
-      console.log(error);
-    }
+    const res = await axios.get(process.env.INSTAGRAM_KEY);
+    setFeed(res.data.data);
   };
 
   useEffect(() => {
